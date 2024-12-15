3b64f8dd29a39f84c2464dcec70f710d96b4cee5
Sindhu9180 <sindhujavr.05@gmail.com>
2024-12-15 12:00:33 +0530

    reactjs code completed

---
+++ myapp1/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+function App() {
+  return (
+    <div>
+      <h1>welcome to reactjs</h1>
+      <h2>hello</h2>
+    </div>
+  )
+}
+export default App
+//if anythig newly added and should be shown in output then use:
+// cd foldername
+//cd filename
+//npm run dev
